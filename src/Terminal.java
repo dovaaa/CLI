d00ef3b96055a8ca45b2628d177718dbaa7006b3
dovaaa <davidemadphilip@gmail.com>
2021-11-12 13:02:10 +0200

    Parser

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,27 +1,45 @@
+import java.util.ArrayList;
+
 class Parser{
     String commandName;
-
+    String[] args;
 
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
-    public boolean parse(String input){}
+    public boolean parse(String input){
+        if(input.equals(""))return false;
+        String[] list = input.split(" ");
+        commandName=list[0];
+        for (int i = 1; i < list.length; i++) {
+            args[i]=list[i];
+        }
+        return true;
+    }
 
-    public String getCommandName(){}
+    public String getCommandName(){
+        return commandName;
+    }
 
-    public String[] getArgs(){}
+    public String[] getArgs(){return args;}
 }
 
 public class Terminal {
     Parser parser;
 
     //Implement each command in a method, for example:
-    public String pwd(){...}
-    public void cd(String[] args){...}
+    public String pwd() {
+
+    }
+    public void cd(String[] args) {
+
+    }
 
 
 
     //This method will choose the suitable command method to be called
-    public void chooseCommandAction(){...}
+    public void chooseCommandAction(){
 
-    public static void main(String[] args){...}
+    }
+
+    public static void main(String[] args){}
 }
