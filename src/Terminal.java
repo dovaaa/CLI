36a1c18f0672fa615b0cdb1a17612cbc2218be79
dovaaa <davidemadphilip@gmail.com>
2021-11-12 14:00:04 +0200

    Started chooseCommandAction

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,28 +1,11 @@
-
 import java.util.ArrayList;
 
 class Parser{
-
     String commandName;
     String[] args;
 
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
-
-
-
-   /* public boolean parse(String input){}
-
-    public String getCommandName(){
-
-    }
-
-    public String[] getArgs(){
-
-    }
-*/
-
-
     public boolean parse(String input){
         if(input.equals(""))return false;
         String[] list = input.split(" ");
@@ -38,22 +21,12 @@ class Parser{
     }
 
     public String[] getArgs(){return args;}
-
 }
 
 public class Terminal {
     Parser parser;
 
     //Implement each command in a method, for example:
-
-   /* public String pwd(){
-
-    }
-    public void cd(String[] args){
-
-    }
-
-
     public String pwd() {
 
     }
@@ -63,24 +36,39 @@ public class Terminal {
 
 
 
-
     //This method will choose the suitable command method to be called
     public void chooseCommandAction(){
+        String command=parser.getCommandName();
+        String[] args=parser.getArgs();
+
+        switch (command){
+            case "echo":
+            //    echo(args[0]);
+                break;
+            case "pwd":
+                pwd();
+                break;
+            case "ls":
+                break;
+            case "mkdir":
+                break;
+            case "rmdir":
+                break;
+            case "touch":
+                break;
+            case "cp":
+                break;
+            case "rm":
+                break;
+            case "cat":
+                break;
+            default:
+                System.out.println("No valid Command");
+
+        }
 
     }
-
-
-    public static void main(String[] args){}
-
-*/
-      Terminal () {
-      }
-
-
-    public static void main(String[] args){
-
+    public void argsCheck(String[] args) {
     }
-
     public static void main(String[] args){}
-
 }
