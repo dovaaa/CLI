d672e6db417a90d42ac58175df0480ab565fa316
Mostafa Mahmoud <mostafamorsy22@gmail.com>
2021-11-12 13:14:44 +0200

    updating

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,27 +1,45 @@
-class Parser{
+class Parser {
     String commandName;
 
 
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
-    public boolean parse(String input){}
+   /* public boolean parse(String input){}
 
-    public String getCommandName(){}
+    public String getCommandName(){
 
-    public String[] getArgs(){}
+    }
+
+    public String[] getArgs(){
+
+    }
+*/
 }
 
 public class Terminal {
     Parser parser;
 
     //Implement each command in a method, for example:
-    public String pwd(){...}
-    public void cd(String[] args){...}
+   /* public String pwd(){
+
+    }
+    public void cd(String[] args){
+
+    }
+
 
 
 
     //This method will choose the suitable command method to be called
-    public void chooseCommandAction(){...}
+    public void chooseCommandAction(){
 
-    public static void main(String[] args){...}
+    }
+*/
+      Terminal () {
+      }
+
+
+    public static void main(String[] args){
+
+    }
 }
